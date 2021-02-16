d7df6cba3d40adc17f8220633195b60535727d4c
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-02-16 17:37:01 +0530

    Login signup running

--- index.js
+++ index.js
@@ -1,47 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 //Importing routes
-const authRoute = require('./routes/auth');
+const authRoute = require("./routes/auth");
 
-//dotenv.config();
-
-//connect to DB
 mongoose.connect(
-    'mongodb+srv://shubham:gharat123@cluster0.dgvqs.mongodb.net/assap?retryWrites=true&w=majority',
-
-() => console.log('connected to db')
+  "mongodb+srv://anurag:anurag@medichain.9zhne.mongodb.net/medichaindb?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Db Connected")
 );
-
-// mongoose.connect('mongodb+srv://shubham:gharat123@cluster0.dgvqs.mongodb.net/assap?retryWrites=true&w=majority',
-// { useNewUrlParser: true,useUnifiedTopology: true  },()=>{
-//     console.log('Connected to DB')
-// })
-
-
-
-//neww
-// mongoose.connect("mongodb+srv://shubham:gharat123@cluster0.dgvqs.mongodb.net/<dbName>?retryWrites=true&w=majority",{
-//     dbName: "assap",
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//     })
-//     .then((result)=>{
-//     console.log("Connected to Mongo")
-//     })  
-//     .catch((error)=>{
-//     console.log(error);
-// })
 
 //middleware
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Main Database");
+});
 
 //Route middlewares
-app.use('/api/user', authRoute);
+app.use("/api/user", authRoute);
 
-app.listen(3000, () => console.log('Server up and running'));
+app.listen(4000, () => console.log("Server up and running"));
