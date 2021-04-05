15304d198f2173277f4fc44e45beb908eb4b36a6
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-04-05 13:08:01 +0530

    ports changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv/config");
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3002;
 //cors policy
 app.use(cors());
 //Importing routes
